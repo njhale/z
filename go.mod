ac7a8b70c0df0a0ce52a208281c05bccce3c4c50
Nick Hale <4175918+njhale@users.noreply.github.com>
2023-11-02 00:07:23 -0400

    Add zgorm.Logger to use logrus with gorm
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/acorn-io/z
 
 go 1.20
+
+require (
+	github.com/sirupsen/logrus v1.9.3
+	gorm.io/gorm v1.25.5
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ zgorm/zgorm.go
@@ -0,0 +1,107 @@
+// Package zgorm exposes common utilities for working with `github.com/go-gorm/gorm`.
+package zgorm
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+	glogger "gorm.io/gorm/logger"
+	gutils "gorm.io/gorm/utils"
+)
+
+// Config is used to configure a gorm Logger that wraps a logrus.Logger.
+type Config struct {
+	// Logger is the logrus logger to use. If nil, logrus.StandardLogger() is used.
+	Logger *logrus.Logger
+
+	// SlowThreshold is the threshold for logging slow queries. If zero, 500ms is used.
+	SlowThreshold time.Duration
+
+	// IgnoreRecordNotFoundError determines if `gorm.ErrRecordNotFound` errors are logged.
+	// `gorm.ErrRecordNotFound` logging is disabled IFF IgnoreRecordNotFoundError is true.
+	IgnoreRecordNotFoundError bool
+}
+
+// NewLogger returns a new *Logger configured with the given config.
+func NewLogger(cfg Config) *Logger {
+	return &Logger{
+		logger:                    cfg.Logger,
+		slowThreshold:             cfg.SlowThreshold,
+		ignoreRecordNotFoundError: cfg.IgnoreRecordNotFoundError,
+	}
+}
+
+// Logger is a gorm logger that wraps a logrus.Logger.
+// The zero value of Logger is valid and writes to logrus.StandardLogger() with default settings.
+type Logger struct {
+	logger                    *logrus.Logger
+	once                      sync.Once
+	slowThreshold             time.Duration
+	ignoreRecordNotFoundError bool
+}
+
+func (l *Logger) LogMode(glogger.LogLevel) glogger.Interface {
+	l.complete()
+	return l
+}
+
+func (l *Logger) Info(ctx context.Context, s string, args ...any) {
+	l.complete()
+	l.logger.WithContext(ctx).Infof(s, args...)
+}
+
+func (l *Logger) Warn(ctx context.Context, s string, args ...any) {
+	l.complete()
+	l.logger.WithContext(ctx).Warnf(s, args...)
+}
+
+func (l *Logger) Error(ctx context.Context, s string, args ...any) {
+	l.complete()
+	l.logger.WithContext(ctx).Errorf(s, args...)
+}
+
+func (l *Logger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	l.complete()
+	elapsed := time.Since(begin)
+	sql, affected := fc()
+
+	log := l.logger.WithContext(ctx).WithFields(logrus.Fields{
+		"elapsed":  elapsed,
+		"affected": affected,
+		"caller":   gutils.FileWithLineNum(),
+	})
+
+	if l.logger.IsLevelEnabled(logrus.TraceLevel) {
+		// Add the SQL query to all log levels if the logger is set to Trace.
+		log = log.WithField("sql", sql)
+	}
+
+	if err != nil && !(l.ignoreRecordNotFoundError && errors.Is(err, gorm.ErrRecordNotFound)) {
+		log.WithError(err).Error("sql query error")
+		return
+	}
+
+	if l.slowThreshold != 0 && elapsed > l.slowThreshold {
+		log.Info("sql query slow")
+		return
+	}
+
+	log.Debug("sql query executed")
+}
+
+// complete ensures that the Logger is fully initialized.
+// It's idempotent and should be called at the beginning of every method exported by Logger.
+func (l *Logger) complete() {
+	l.once.Do(func() {
+		if l.logger == nil {
+			l.logger = logrus.StandardLogger()
+		}
+		if l.slowThreshold == 0 {
+			l.slowThreshold = 500 * time.Millisecond
+		}
+	})
+}
